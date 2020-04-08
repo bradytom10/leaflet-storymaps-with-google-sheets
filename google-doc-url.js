1b77591bec55a22c45a5be8b06d3f86e93eff2e1
bradytom10 <63370891+bradytom10@users.noreply.github.com>
2020-04-08 21:07:43 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/12DTCaQuoJUw2TAknV5E5xMiHkMZ_OXslsAhTIYowYIk/edit#gid=0';
